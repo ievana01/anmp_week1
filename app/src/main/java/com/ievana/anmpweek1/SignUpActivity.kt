8d3f98436b18e2d6da376d49b97152e1af774c42
ievana01 <118661162+ievana01@users.noreply.github.com>
2024-02-22 14:28:22 +0700

    sign up new

--- app/src/main/java/com/ievana/anmpweek1/SignUpActivity.kt
+++ app/src/main/java/com/ievana/anmpweek1/SignUpActivity.kt
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class SignUpActivity : AppCompatActivity() {
+
+    //sign up for user
+    // 1. cek existing account
+    // 2. cek password
+    // 3. implement otp
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
